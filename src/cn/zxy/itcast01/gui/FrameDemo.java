c0072170677566bdbc3133a58586b74085fc53f3
ZhouXiaoYue <2906447412@qq.com>
2020-05-11 20:36:21 +0800

    第一次提交项目gui

---
+++ src/cn/zxy/itcast01/gui/FrameDemo.java
@@ -0,0 +1,10 @@
+package cn.zxy.itcast01.gui;
+
+import java.awt.*;
+
+public class FrameDemo {
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setVisible(true);
+    }
+}
---
+++ src/cn/zxy/itcast01/gui/Student.java
@@ -0,0 +1,10 @@
+package cn.zxy.itcast01.gui;
+
+public class Student {
+    private String name;
+    private String school;
+    public void setName(String name,String school){
+        this.name = name;
+        this.school = school;
+    }
+}
